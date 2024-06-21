5b6d4b4a7c2d9b840bfdccef619ad678992b7554
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-06-21 13:59:27 +0530

    complete functional Trainer Page

---
+++ src/Assets/images/FunctionalTrainer.js
@@ -0,0 +1,135 @@
+import FT_1 from './FT_1.png'
+import FT_2 from './FT_2.png'
+import FT_3 from './FT_3.png'
+import FT_4 from './FT_4.png'
+import FT_5 from './FT_5.png'
+import FT_6 from './FT_6.png'
+import FT_7 from './FT_7.png'
+import FT_8 from './FT_8.png'
+import FT_9 from './FT_9.png'
+import FT_10 from './FT_10.png'
+import FT_11 from './FT_11.png'
+import FT_12 from './FT_12.png'
+import FT_13 from './FT_13.png'
+import FT_14 from './FT_14.png'
+import FT_15 from './FT_15.png'
+import FT_16 from './FT_16.png'
+
+
+const FunctionalTrainingImage  = [
+    {
+        src: FT_1,
+        alt: 'Image',
+        description : 'PROFORMANCE PLUS FUNCTIONAL TRAINER (PPMS-245)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_2,
+        alt: 'Image',
+        description : 'TUFFSTUFF SIX-PAK FUNCTIONAL TRAINER (SPT-7)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_3,
+        alt: 'Image',
+        description : 'FUNCTIONAL TRAINER',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_4,
+        alt: 'Image',
+        description : 'LIFE FITNESS SIGNATURE SERIES CABLE COLUMN',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_5,
+        alt: 'Image',
+        description : 'IN10CT XL300 FUNCTIONAL TRAINER',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_6,
+        alt: 'Image',
+        description : 'TUFFSTUFF SIX-PAK FUNCTIONAL TRAINER (SPT-6X)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_7,
+        alt: 'Image',
+        description : 'MULTIFUNCTIONAL DUAL PULLEY WITH SMITH ATTACHMENT',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_8,
+        alt: 'Image',
+        description : 'PARAMOUNT FUNCTIONAL TRAINER',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_9,
+        alt: 'Image',
+        description : 'TUFFSTUFF EVOLUTION DUAL ADJUSTABLE PULLEY SYSTEM',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_10,
+        alt: 'Image',
+        description : 'TUFFSTUFF EVOLUTION DUAL STACK FUNCTIONAL TRAINER (MFT-2700)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_11,
+        alt: 'Image',
+        description : 'TUFFSTUFF EVOLUTION CORNER MULTI-FUNCTIONAL TRAINER CXT-200',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_12,
+        alt: 'Image',
+        description : 'PROFORMANCE PLUS COMPACT CROSSOVER (PPMS-255)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_13,
+        alt: 'Image',
+        description : 'TUFFSTUFF DUAL STACK FUNCTIONAL TRAINER (HTX-2000)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_14,
+        alt: 'Image',
+        description : 'TUFFSTUFF SIX-PAK BASE FUNCTIONAL TRAINER (SPT-6B)',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_15,
+        alt: 'Image',
+        description : 'PARAMOUNT PFT-200 FUNCTIONAL TRAINER WITH DUAL ADJUSTABLE PULLEYS',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+    {
+        src: FT_16,
+        alt: 'Image',
+        description : 'LIFE FITNESS DUAL ADJUSTABLE PULLEY',
+        price: '',
+        buttonText : 'ENQUIRE HERE'
+    },
+
+
+];
+export default FunctionalTrainingImage;
--- src/DropDownPages/FunctionalDropDown/FunctionalTrainer.js
+++ src/DropDownPages/FunctionalDropDown/FunctionalTrainer.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import FunctionalTrainingImage from '../../Assets/images/FunctionalTrainer';
+import ProductCategory from '../../Components/CoreComponents/Product Category/ProductCategory';
+import BrandCategory from './../../Components/CoreComponents/Brand Category/BrandCategory';
+import './functionalTrainer.css'; // Import the CSS file
 
 const FunctionalTrainer = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const imagesPerPage = 8;
+
+  // Calculate the indices of the images to show on the current page
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  const currentImages = FunctionalTrainingImage.slice(indexOfFirstImage, indexOfLastImage);
+
+  // Handle pagination
+  const totalPages = Math.ceil(FunctionalTrainingImage.length / imagesPerPage);
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+  const handlePreviousPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
   return (
-    <div>FunctionalTrainer</div>
-  )
-}
+    <div className='ft-top-container-wrapper'>
+      <div className='ft-top-background-wallpaper'>
+        <h1>RIGS</h1>
+      </div>
+      <div className='ft-main-container-wrapper'>
+        <div className='ft-category-container-wrapper'>
+          <div className='ft-product-category-container'>
+            <ProductCategory />
+          </div>
+          <div className='ft-brand-category-container'>
+            <BrandCategory />
+          </div>
+        </div>
+        <div className='ft-image-full-outer-wrapper'>
+        <div className='ft-image-container-wrapper'>
+          {currentImages.map((image, index) => (
+            <div key={index} className='ft-image-container'>
+              <img src={image.src} alt={image.alt} className="ft-gallery-image" />
+              <p className="ft-image-description">{image.description}</p>
+              <p className="ft-product-price">{image.price}</p>
+              <button className="ft-enquire-button">{image.buttonText}</button>
+            </div>
+          ))}
+        </div>
+        <div className="ft-pagination">
+          <button onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+          <span>Page {currentPage} of {totalPages}</span>
+          <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default FunctionalTrainer;
