01ea4ae71b0840b2a0bcc30b714fabef2e77352c
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-06-21 11:07:33 +0530

    complete Rigs  pages

---
+++ src/Assets/images/Rigs.js
@@ -0,0 +1,97 @@
+import Rigs_1 from './../images/Rigs_1.png'
+import Rigs_2 from './../images/Rigs_2.png'
+import Rigs_3 from './../images/Rigs_3.png'
+import Rigs_4 from './../images/Rigs_4.png'
+import Rigs_5 from './../images/Rigs_5.png'
+import Rigs_6 from './../images/Rigs_6.png'
+import Rigs_7 from './../images/Rigs_7.png'
+import Rigs_8 from './../images/Rigs_8.png'
+import Rigs_9 from './../images/Rigs_9.png'
+import Rigs_10 from './../images/Rigs_10.png'
+import Rigs_11 from './../images/Rigs_11.png'
+import Rigs_12 from './../images/Rigs_12.png'
+import Rigs_13 from './../images/Rigs_13.png'
+
+
+const RigsImage = [
+    {
+        src: Rigs_1,
+        alt: "Images",
+        description: 'TUFFSTUFF PROFORMANCE PLUS PRO-XXL ATHLETIC RACK SYSTEM',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_2,
+        alt: "Images",
+        description: 'ALPHA FIT 3 CELL RIG',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_3,
+        alt: "Images",
+        description: 'IRON EDGE FUNCTIONAL RIG',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_4,
+        alt: "Images",
+        description: 'GYMRAX FREE ANCHOR SYSTEM',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_5,
+        alt: "Images",
+        description: 'GYMRAX BRIDGE OPTIONS',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_6,
+        alt: "Images",
+        description: 'GYMRAX STORAGE BAY OPTIONS',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_7,
+        alt: "Images",
+        description: 'GYMRAX SQUAT + DOUBLE BAY STORAGE',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_8,
+        alt: "Images",
+        description: 'GYMRAX FUNCTIONAL CORNER',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_9,
+        alt: "Images",
+        description: 'GYMRAX BI-DIRECTIONAL',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_10,
+        alt: "Images",
+        description: 'GYMRAX TRIPLE BAY SUSPENSION',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_11,
+        alt: "Images",
+        description: 'GYMRAX DOUBLE BAY SUSPENSION',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_12,
+        alt: "Images",
+        description: 'GYMRAX SINGLE BAY SUSPENSION',
+        buttonText: "ENQUIRE HERE",
+    },
+    {
+        src: Rigs_13,
+        alt: "Images",
+        description: 'QUEENAX FUNCTIONAL TRAINING RIG',
+        buttonText: "ENQUIRE HERE",
+    },
+
+];
+export default RigsImage;
--- src/DropDownPages/FunctionalDropDown/Rigs.js
+++ src/DropDownPages/FunctionalDropDown/Rigs.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import  './rigs.css'
+import ProductCategory from '../../Components/CoreComponents/Product Category/ProductCategory';
+import BrandCategory from '../../Components/CoreComponents/Brand Category/BrandCategory';
+import RigsImage from '../../Assets/images/Rigs';
 
 const Rigs = () => {
   return (
-    <div>Rigs</div>
+    <div className='rigs-top-container-wrapper'>
+    <div className='rigs-top-background-wallpaper'>
+      <h1>RIGS</h1>
+    </div>
+    <div className='rigs-main-container-wrapper'>
+      <div className='rigs-category-container-wrapper'>
+        <div className='rigs-product-category-container'>
+          <ProductCategory />
+        </div>
+        <div className='rigs-brand-category-container'>
+          <BrandCategory />
+        </div>
+      </div>
+      <div className='rigs-image-container-wrapper'>
+        {RigsImage.map((image, index) => (
+          <div key={index} className='rigs-image-container-wrapper'>
+            <img src={image.src} alt={image.alt} className="rigs-gallery-image" />
+            <p className="rigs-image-description">{image.description}</p>
+            <p className="rigs-product-price">{image.price}</p>
+            <button className="rigs-enquire-button">{image.buttonText}</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
   )
 }
 
