fd1afca1556d630766e80ebacd9dd63acfcb1511
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-06-21 15:28:51 +0530

    complete functional Cardio Page

---
+++ src/Assets/ImageArray/FunctionalCardio.js
@@ -0,0 +1,61 @@
+import Functional_Cardio_1 from './../images/Functional_Cardio_1.png'
+import Functional_Cardio_2 from './../images/Functional_Cardio_2.png'
+import Functional_Cardio_3 from './../images/Functional_Cardio_3.png'
+import Functional_Cardio_4 from './../images/Functional_Cardio_4.png'
+import Functional_Cardio_5 from './../images/Functional_Cardio_5.png'
+import Functional_Cardio_6 from './../images/Functional_Cardio_6.png'
+import Functional_Cardio_7 from './../images/Functional_Cardio_7.png'
+
+const FunctionalCardioImage = [
+{
+    src : Functional_Cardio_1,
+    alt : 'Image',
+    description : 'IN10CT HEALTH RUNNER CURVED TREADMILL',
+    price : '$3,999.00',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_2,
+    alt : 'Image',
+    description : 'IN10CT RESISTANCE RUNNER CURVED TREADMILL',
+    price : '$6,666.0',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_3,
+    alt : 'Image',
+    description : 'CONCEPT2 ROWER W/ PM5 MONITOR',
+    price : '$5,899.0',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_4,
+    alt : 'Image',
+    description : 'ROGUE ECHO BIKE',
+    price : '$4,584.0',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_5,
+    alt : 'Image',
+    description : 'TECHNOGYM SKILLMILL',
+    price : '$6,000.00',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_6,
+    alt : 'Image',
+    description : 'CARDIOGYM CG6',
+    price : '$6,399.00',
+    buttonText : 'ENQUIRE HERE'
+},
+{
+    src : Functional_Cardio_7,
+    alt : 'Image',
+    description : 'PRO 6 POWER PLATE',
+    price : '$4,999.0',
+    buttonText : 'ENQUIRE HERE'
+},
+];
+
+export default FunctionalCardioImage;
