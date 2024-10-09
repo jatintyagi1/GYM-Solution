09371ba595f0bf4460ca040b5e1ec34ab5c0efa5
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-09 11:04:53 +0530

    remove unused code

--- src/Components/Signin/signin.jsx
+++ src/Components/Signin/signin.jsx
@@ -9,15 +9,8 @@ import '../../Styles/User/Signup.css'
 
 
 const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-    confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
-        .required('Confirm Password is required'),
-    mobileNumber: Yup.string()
-        .matches(/^[0-9]{10}$/, 'Mobile number must be 10 digits')
-        .required('Mobile number is required')
 });
 
 export default function SignIn() {
