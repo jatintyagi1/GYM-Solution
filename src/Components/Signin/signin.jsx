5bf2fbaaddebb34011567911487399461e626c9d
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-18 10:46:08 +0530

    update some Login page code

--- src/Components/Signin/signin.jsx
+++ src/Components/Signin/signin.jsx
@@ -4,6 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock, faUser, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 import '../../Styles/User/Signup.css'
 
@@ -59,6 +60,9 @@ export default function SignIn() {
                     />
                 </div>
                 <div className="invalid-feedback">{errors.password?.message}</div>
+            </div>
+            <div className='login-forgot'>
+               <Link to='/forgot-password'><p>Forgot Password ?</p></Link> 
             </div>
             <button type="submit" className="btn btn-signup">Login In</button>
         <div className='login-text'>
