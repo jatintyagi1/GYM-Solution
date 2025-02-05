4f499cc9b54c7d23ac5f1ab6b02050b2f2a81448
Jatin Tyagi <tyagijatin6263@gmail.com>
2025-02-05 20:31:32 +0530

    fix loading and enhance app

--- src/index.js
+++ src/index.js
@@ -8,36 +8,12 @@ import LoadingSpinner from './Components/Loading';
 
 const App = React.lazy(() => import('./App'));
 
-const RootComponent = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a 2-second loading time
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    // Cleanup the timer
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <Suspense fallback={<LoadingSpinner />}>
-          <App />
-        </Suspense>
-      )}
-    </>
-  );
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RootComponent />
+    <Suspense fallback={<LoadingSpinner />}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
 
